ec02c5847d10ecd28bafc4e7ddf6db58bdb2e411
tomoya8 <gea01051@yahoo.co.jp>
2024-09-29 22:27:56 +0900

    Add help and progress bar

--- const.py
+++ const.py
@@ -14,8 +14,14 @@
 #
 
 OMR_IMAGE_PROCESSING_WIDTH = 2000
+IGNORE_FRAMES_SMALLER_THAN = 0.02
 
-MARK = ("1", "2", "3", "4", "5", "6", "7", "8", "9", "0")
+# aspect ratio of mark
+DEFAULT_MARK_SMALL_LIM = 0.4
+MARK_MIN_AR = 0.5
+MARK_MAX_AR = 2.0
+
+MARK_LAYOUT = ("1", "2", "3", "4", "5", "6", "7", "8", "9", "0")
 
 STYLE = """
 <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0/css/all.min.css" />
@@ -32,7 +38,7 @@ STYLE = """
 RESULTS = """
 ### <i class="far fa-angle-double-down"></i> ダウンロード
 
-- <a href="data:file/csv;base64,{csv_b64}" download="result.csv">マーク結果一覧</a> <i class="fas fa-file-csv fa-lg my-green"></i>
-- <a href="data:file/pdf;base64,{pdf_b64}" download="result.pdf">マーク検出画像</a> <i class="fas fa-file-pdf fa-lg my-red"></i>
+- <a href="data:file/csv;base64,{csv_b64}" download="{csv_name}">マーク結果一覧</a> <i class="fas fa-file-csv fa-lg my-green"></i>
+- <a href="data:file/pdf;base64,{pdf_b64}" download="{pdf_name}">マーク検出画像</a> <i class="fas fa-file-pdf fa-lg my-red"></i>
 
 """
